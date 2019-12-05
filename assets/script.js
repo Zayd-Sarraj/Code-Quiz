e693d818f36b9c736ddb484fffe54c1e194e25b3
Zayd Sarraj <zayd.sarraj@gmail.com>
2019-12-05 02:05:13 -0500

    coded the quiz starting page html, psuedocoded js

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+// Setting variables for html elements and the questions array 
+
+// Clicking start button starts the quiz
+
+// Title row changes to questions
+
+// Description row changes to answers, which are buttons
+
+// Timer Begins
+
+// If user clicks the correct answer, they gain score and move on to the next question
+
+// If user clicks any other answer, timer loses 10 seconds and user moves on to the next question
+
+// If there are no more questions or time runs out the quiz ends and asks the user to input their name for highscores
+
+// After input of name or if 'view highscores' is clicked, highscores are displayed 
+
+// buttons to clear highscores or start game over
+
+// store highscores to local storage 
